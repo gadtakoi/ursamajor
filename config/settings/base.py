1824bb2c0e4a2c8316ff4669220155a2761f4417
stas <gadtakoi@gmail.com>
2020-05-02 19:42:13 +0300

    Static setup

--- config/settings/base.py
+++ config/settings/base.py
@@ -82,7 +82,7 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/tmp/static/'
+STATIC_ROOT = '/opt/ursamajor_static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, '../media')
